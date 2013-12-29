fba0a8c7d35cc1d63eab36467015736a7d2736b3
sunorry <sunorry@gmail.com>
2013-12-29 21:46:59 +0800

    add exports.change method to export port(in annotion)

--- js/change.js
+++ js/change.js
@@ -11,4 +11,8 @@ define(function(require, exports, module){
 
   //暴露接口
   module.exports = change;
+
+  //第二种暴露接口的方法,这种方法暴露的话引用的方法就不同了，详细看 test.js 下的注释代码
+  //exports.change = change;
+
 })
--- js/test.js
+++ js/test.js
@@ -1,9 +1,18 @@
 define(function(require, exports, mudule){
-  var $ = require('jquery');
+  require('jquery');  //引入 jquery
+
+
   var change = require('change');
 
   console.log(jQuery);
   change.color('oOl', 'blue');
   change.font('oOl', '22px');
+
+  //用 exports.change = change; 的调用方法, 可能是我用的很丑， var change = require('change'); 里的change 就相当于 exports，所以 change.change 就是 exports.change
+
+  // var change = require('change');
+  // change.change.color('oOl', 'blue');
+  // change.change.font('oOl', '22px');
+
 
 })
